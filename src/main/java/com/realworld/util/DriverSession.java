efcf28800f6be97f2dfb8391666c95ee780ef668
Dat Nguyen <dat.n8n@gmail.com>
2022-03-27 09:37:01 +0700

    remove singleton and implement different way to run test parallel

--- src/main/java/com/realworld/util/DriverSession.java
+++ src/main/java/com/realworld/util/DriverSession.java
@@ -3,27 +3,11 @@ package com.realworld.util;
 import org.openqa.selenium.WebDriver;
 
 public class DriverSession {
-    private static WebDriver driver;
-    private static DriverSession instance;
-    private DriverSession(WebDriver driver) {
-        this.driver = driver;
+    private static ThreadLocal<WebDriver> driver = new ThreadLocal<>();
+    public DriverSession(WebDriver driver) {
+        this.driver.set(driver);
     }
-    public static WebDriver getDriver() {
-        return driver;
-    }
-
-    public static DriverSession getInstance(WebDriver driver) {
-        if (instance == null) {
-            instance = new DriverSession(driver);
-        }
-        return instance;
-    }
-
-    public static DriverSession quitDriver(WebDriver driver) {
-        if(instance != null) {
-            driver.quit();
-            instance = null;
-        }
-        return null;
+    public static synchronized WebDriver getDriver() {
+        return driver.get();
     }
 }
--- src/test/java/com/realworld/tests/BaseTest.java
+++ src/test/java/com/realworld/tests/BaseTest.java
@@ -7,29 +7,36 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.*;
 
 public class BaseTest {
-    private DriverSession driverSession;
-    private WebDriver driver;
+    private DriverSession driverSessionHolder;
+    private ThreadLocal<WebDriver> driver = new ThreadLocal<>();
     @BeforeTest
     public void setupProperty() {
         BasicConfigurator.configure();
-        System.setProperty("webdriver.chrome.driver",
-                "C:\\selenium\\drivers\\chromedriver_win32\\chromedriver.exe");
-        System.setProperty("webdriver.chrome.logfile", "C:\\selenium\\logs\\chromedriver.log");
     }
 
     @BeforeMethod
-    public void setupDriver() {
-        this.driverSession = DriverSession.getInstance(new ChromeDriver());
-        this.driver = DriverSession.getDriver();
-        this.driver.manage().window().maximize();
+    @Parameters("browser")
+    public void setupDriver(String browser) {
+        if(browser.equalsIgnoreCase("chrome")) {
+            System.setProperty("webdriver.chrome.driver",
+                    "C:\\selenium\\drivers\\chromedriver_win32\\chromedriver.exe");
+            System.setProperty("webdriver.chrome.logfile", "C:\\selenium\\logs\\chromedriver.log");
+            this.driverSessionHolder = new DriverSession(new ChromeDriver());
+        }
+        this.driver.set(DriverSession.getDriver());
+        getDriver().manage().window().maximize();
+    }
+
+    private synchronized WebDriver getDriver() {
+        return driver.get();
     }
 
     @AfterMethod
-    public void quitDriver() {
-        DriverSession.quitDriver(driver);
+    public void tearDown() {
+        getDriver().quit();
     }
 
     protected void navigateToPage(String url) {
-        this.driver.get(url);
+        getDriver().get(url);
     }
 }
